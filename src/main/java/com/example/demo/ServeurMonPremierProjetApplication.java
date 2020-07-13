f2a74ce14b54fb61b636d4ce447ca36b3fc06f37
unknown <gagoumrachelle@gmail.com>
2020-07-13 13:38:42 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServeurMonPremierProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurMonPremierProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurMonPremierProjetApplication.class, args);
+	}
+
+}
